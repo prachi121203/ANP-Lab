d24b1f804947c852fc331de3baac209183a3e5f4
Prachi Kumari <151460250+prachi121203@users.noreply.github.com>
2025-06-26 21:47:28 +0530

    Update even_odd.py

--- even_odd.py
+++ even_odd.py
@@ -1,15 +1,5 @@
-number = int(input("Enter a No."))
-
-if number <= 1:
-    print(f"{number} is not a prime number.")
-else:
-    is_prime = True
-    for i in range(2, number):
-        if number % i == 0:
-            is_prime = False
-            break
-
-    if is_prime:
-        print(f"{number} is a prime number.")
-    else:
-        print(f"{number} is not a prime number.")
+num = int(input("Enter a Number:")) 
+if num % 2 == 0: 
+  print("Given number is Even") 
+else: 
+  print("Given number is Odd")
